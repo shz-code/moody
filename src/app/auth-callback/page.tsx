dce486cc0000b74dbe94e55753565c02083137b8
shz-code <web.shahidul.alam@gmail.com>
2024-10-12 16:15:58 +0600

    add: Setup prisma and user model

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
 
+import { Loader2 } from "lucide-react";
 import { trpc } from "../_trpc/client";
 
 const Page = () => {
@@ -9,8 +10,38 @@ const Page = () => {
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  const { data } = trpc.test.useQuery();
+  const { data, isLoading, isError, error } = trpc.authCallback.useQuery(
+    undefined,
+    { retry: true, retryDelay: 500 }
+  );
 
-  return <div>{data}</div>;
+  if (isLoading)
+    return (
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+          <h3 className="font-semibold text-xl">Setting up your account...</h3>
+          <p>You will be redirected automatically.</p>
+        </div>
+      </div>
+    );
+
+  if (isError) {
+    if (error.data?.code === "UNAUTHORIZED")
+      return (
+        <div className="w-full mt-24 flex justify-center">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+            <h3 className="font-semibold text-xl">
+              There was an error setting up your account...
+            </h3>
+            <p>Please login again.</p>
+          </div>
+        </div>
+      );
+  }
+
+  // return <div>ok</div>;
+  return data?.success && router.push(origin ? `/${origin}` : "/dashboard");
 };
 export default Page;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,30 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { procedure, router } from "./trpc";
 
 export const appRouter = router({
-  test: procedure.query(() => {
-    return "hello";
+  authCallback: procedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    // Check if user is in db
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser)
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    return { success: true };
   }),
 });
 // export type definition of API
