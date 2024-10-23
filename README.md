5c915e837aff3f4dd6094e1b7add3fce6a112f95
shz-code <web.shahidul.alam@gmail.com>
2024-10-24 01:18:59 +0600

    modify: Readme (add useful links)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Moody - AI powered pdf reader
+
+# Useful links
+
+- https://github.com/vercel/next.js/discussions/67501 (Steam Response)
+- https://github.com/google-gemini/generative-ai-js/blob/927f6f0d11e8fde9c53c115099e7f01558f5570a/samples/chat.js (Google AI Chat sample code)
+- https://js.langchain.com/docs/integrations/vectorstores/pinecone/ (Pinecone langchain integration)
--- src/components/ChatWrapper/ChatInput.tsx
+++ src/components/ChatWrapper/ChatInput.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Send } from "lucide-react";
-import { FC, useContext } from "react";
+import { FC, useContext, useRef } from "react";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { ChatContext } from "./ChatContext";
@@ -10,7 +10,10 @@ interface ChatInputProps {
 }
 
 const ChatInput: FC<ChatInputProps> = ({ isDisabled }) => {
-  const { addMessage, handleInputChange } = useContext(ChatContext);
+  const { message, addMessage, handleInputChange, isLoading } =
+    useContext(ChatContext);
+
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
   return (
     <div className="absolute bottom-0 left-0 w-full">
       <div className="mx-2 flex flex-row md:mx-4 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
@@ -18,19 +21,31 @@ const ChatInput: FC<ChatInputProps> = ({ isDisabled }) => {
           <div className="relative flex flex-col w-full flex-grow p-4">
             <div className="relative">
               <Textarea
+                ref={textAreaRef}
                 rows={1}
                 maxRows={4}
                 autoFocus
                 placeholder="Enter your question..."
                 className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
                 onChange={(e) => handleInputChange(e)}
+                value={message}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    addMessage();
+                    textAreaRef.current?.focus();
+                  }
+                }}
               />
 
               <Button
+                disabled={isLoading || isDisabled}
+                type="submit"
                 className="absolute bottom-1.5 right-[8px]"
                 aria-label="send message"
                 onClick={() => {
                   addMessage();
+                  textAreaRef.current?.focus();
                 }}
               >
                 <Send className="h-4 w-4" />
